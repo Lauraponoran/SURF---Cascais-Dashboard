678ea89020055890eea19fcc5c6a542626d2cbfd
Lauraponoran <lauraponoran7@gmail.com>
2026-09-21 09:04:55 +0200

    fix city center

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ export const CONFIG = {
   MAP_STYLE: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
 
   // TODO: set to your city's center [lng, lat].
-  MAP_CENTER: [9.7386, 52.3744],
+  MAP_CENTER: [9.4218, 38.6973],
   MAP_ZOOM: 13,
 
   // Folder the map fetches trips.geojson / road_segments_averaged.json from.
